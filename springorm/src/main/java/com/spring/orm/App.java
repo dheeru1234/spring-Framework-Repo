474081c3ff94698319a2bdb7a3488ab036747dcf
dheeru1234 <tiwaridheeru907@gmail.com>
2023-01-31 23:10:41 +0530

    initial commit

---
+++ springorm/src/main/java/com/spring/orm/App.java
@@ -0,0 +1,21 @@
+package com.spring.orm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.orm.dao.StudentDao;
+import com.spring.orm.entities.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+		Student student = new Student(201103, "Dharmendra Tiwari", "Ballia");
+		int r = studentDao.insert(student);
+		System.out.println("done " + r);
+	}
+}
